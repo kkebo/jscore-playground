cd0cf4810ad899cc60173020f37ce396d127cf55
Kenta Kubo <kkk669@users.noreply.github.com>
2017-10-09 18:32:12 +0900

    Replace UITextView with UITableView

--- jscore.playground/Contents.swift
+++ jscore.playground/Contents.swift
@@ -4,18 +4,20 @@ import PlaygroundSupport
 Bundle(path: "/System/Library/Frameworks/JavaScriptCore.framework")?.load()
 let JSContextClass = NSClassFromString("JSContext") as? NSObject.Type
 
-class ViewController: UIViewController, UITextFieldDelegate {
+struct Log {
+    public let text: String
+    public let backgroundColor: UIColor?
+}
+
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource {
     var context: NSObject = {
         guard let instance = JSContextClass?.init() as? NSObject else { fatalError() }
         return instance
     }()
-    private let textView: UITextView = {
-        let textView = UITextView()
-        textView.backgroundColor = #colorLiteral(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
-        textView.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        textView.font = UIFont(name: "Menlo", size: 14)
-        textView.isEditable = false
-        return textView
+    private var results: [Log] = [];
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
     }()
     private let prompt: UITextField = {
         let textField = UITextField()
@@ -37,35 +39,58 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         self.context.setValue({ (_, exception: NSObject!) in
             guard let string = exception.perform(Selector("toString")).takeUnretainedValue() as? String else { fatalError() }
-            DispatchQueue.main.async {
-                self.textView.text = self.textView.text.appending(string).appending("\n")
-            }
+            self.appendCell(string, color: #colorLiteral(red: 0.95686274766922, green: 0.658823549747467, blue: 0.545098066329956, alpha: 1.0))
         } as @convention(block) (NSObject?, NSObject?) -> Void, forKey: "exceptionHandler")
         
         self.prompt.delegate = self
-        let stackView = UIStackView(arrangedSubviews: [self.textView, self.prompt])
+        
+        self.tableView.dataSource = self
+        
+        let stackView = UIStackView(arrangedSubviews: [self.tableView, self.prompt])
         stackView.axis = .vertical
         self.view = stackView
     }
     
+    func appendCell(_ text: String, color: UIColor?) {
+        let log = Log(text: text, backgroundColor: color)
+        self.results.append(log)
+        self.tableView.reloadData()
+        self.tableView.scrollToRow(at: IndexPath(row: self.results.count - 1, section: 0), at: .top, animated: true)
+    }
+    
+    // MARK: UITextFieldDelegate
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         guard let script = textField.text else { fatalError() }
         
-        DispatchQueue.main.async {
-            self.textView.text = self.textView.text.appending("> \(script)").appending("\n")
-            self.textView.scrollRangeToVisible(NSMakeRange(self.textView.text?.count ?? 0, 0))
-        }
+        self.appendCell("> \(script)", color: nil)
         
         textField.text?.removeAll()
         let jsValue = self.context.perform(Selector("evaluateScript:"), with: script).takeUnretainedValue()
         guard let result = jsValue.perform(Selector("toString")).takeRetainedValue() as? String else { fatalError() }
         
-        DispatchQueue.main.async {
-            self.textView.text = self.textView.text.appending(result).appending("\n")
-            self.textView.scrollRangeToVisible(NSMakeRange(self.textView.text?.count ?? 0, 0))
-        }
+        self.appendCell(result, color: nil)
         
         return false
+    }
+    
+    // MARK: UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = self.results[indexPath.row]
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.backgroundColor = item.backgroundColor ?? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        cell.textLabel?.font = UIFont(name: "Menlo", size: 14)
+        cell.textLabel?.text = item.text
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.results.count
     }
 }
 
