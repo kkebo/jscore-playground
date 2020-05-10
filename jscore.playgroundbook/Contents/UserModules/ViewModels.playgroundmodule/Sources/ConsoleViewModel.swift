aa5d06bc3d619040d3cf59d2c43401ca5906ec80
Kenta Kubo <kkk669@users.noreply.github.com>
2020-05-10 12:59:23 +0900

    Use WKWebView instead of JavaScriptCore

--- jscore.playgroundbook/Contents/UserModules/ViewModels.playgroundmodule/Sources/ConsoleViewModel.swift
+++ jscore.playgroundbook/Contents/UserModules/ViewModels.playgroundmodule/Sources/ConsoleViewModel.swift
@@ -1,26 +1,38 @@
 import Combine
 import Dispatch
-import JavaScriptCoreWrapper
+import WebKit
 import Models
 
 public class ConsoleViewModel: ObservableObject {
     @Published public var input = ""
     @Published public var messages = [ConsoleMessage]()
 
-    let context = JSContext()
-
-    public init() {
-        self.context.exceptionHandler = { _, exception in
-            let string = exception!.toString()
-            self.messages.append(ConsoleMessage(text: string, type: .error))
-        }
-    }
+    let webView = WKWebView()
+    
+    public init() {}
 
     public func run() {
         self.messages.append(ConsoleMessage(text: self.input, type: .input))
 
-        let result = self.context.evaluateScript(self.input).toString()
-        self.messages.append(ConsoleMessage(text: result, type: .value))
+        self.webView.evaluateJavaScript(self.input) { value, error in
+            if let error = error {
+                self.messages.append(ConsoleMessage(text: error.localizedDescription, type: .error))
+                return
+            }
+            
+            switch value {
+            case let value as String:
+                self.messages.append(ConsoleMessage(text: value, type: .value))
+            case let value as Int:
+                self.messages.append(ConsoleMessage(text: value.description, type: .value))
+            case let value as Double:
+                self.messages.append(ConsoleMessage(text: value.description, type: .value))
+            case let .some(value):
+                self.messages.append(ConsoleMessage(text: "\(value)", type: .value))
+            case let .none:
+                self.messages.append(ConsoleMessage(text: "null", type: .value))
+            }
+        }
 
         self.input = ""
     }
